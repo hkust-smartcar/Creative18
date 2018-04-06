2a8f47e47ecbd169cc8d027becb904874e740442
Peter Tse <tsehonam@gmail.com>
2018-04-07 01:45:15 +0800

    Fixed a bug
    that we also need to check the cycle is continuous

--- hamiltonian.cpp
+++ hamiltonian.cpp
@@ -4,6 +4,8 @@
 #include <deque>
 #include <iterator>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -21,33 +23,33 @@ struct Node {
 
 vector<Entry> owal;
 
-const int size = 5;
+const int chessNo = 11;
 
-void print_owal(const vector<Entry>& owal) {
+void printOwal(const vector<Entry>& owal) {
     for (auto&& e : owal) {
         cout << "w:" << e.weight << " at (" << e.coords.first << "," << e.coords.second << ")" << endl;
     }
 }
 
-void print_com(const vector<int>& a) {
+void printCom(const vector<int>& a) {
     for (auto&& e: a) {
         cout << e << " ";
     }
     cout << endl;
 }
 
-void print_count(const array<int, size>& a) {
+void printCount(const array<int, chessNo>& a) {
     cout << "Count: ";
     for (auto&& e : a)
         cout << e << " ";
     cout << endl;
 }
 
-array<Node, size> genCycle(const array<int, size>& count, const vector<int>& com) {
-    array<int, size> t_count = count;
+array<Node, chessNo> genCycle(const array<int, chessNo>& count, const vector<int>& com) {
+    array<int, chessNo> t_count = count;
 
-    array<Node, size> nodes;
-    for (int i = 1; i <= size; i++) {
+    array<Node, chessNo> nodes;
+    for (int i = 1; i <= chessNo; i++) {
         nodes[i - 1].val = i;
     }
 
@@ -68,15 +70,15 @@ array<Node, size> genCycle(const array<int, size>& count, const vector<int>& com
     return nodes;
 }
 
-array<int, size> genPath(const array<Node, size>& nodes) {
-    array<int, size> path;
-    const Node* n = &nodes[size - 1];
+array<int, chessNo> genPath(const array<Node, chessNo>& nodes) {
+    array<int, chessNo> path;
+    const Node* n = &nodes[chessNo - 1];
     path[0] = n->val;
     int old1 = n->val;
     n = n->next1;
     path[1] = n->val;
     int old2 = n->val;
-    for (int i = 2; i <= size; i++) {
+    for (int i = 2; i <= chessNo; i++) {
         old2 = n->val;
         if (old1 == n->next1->val)
             n = n->next2;
@@ -89,13 +91,20 @@ array<int, size> genPath(const array<Node, size>& nodes) {
     return path;
 };
 
-bool checkCycle(const array<int, size>& cycle) {
+bool checkCycle(const array<int, chessNo>& cycle) {
     bool valid_first = true;
     bool valid_second = true;
+
+    // check if continuous cycle
+    if (find(cycle.begin()+1, cycle.end(), chessNo) != cycle.end()) {
+        return false;
+    }
+
+    
     // ONE DIRECTION
-    const int half = (size - 1) / 2;
-    for (int i = 1, n = 0; i < size; i++) {
-        if (cycle[(i) % size] <= half)
+    const int half = (chessNo - 1) / 2;
+    for (int i = 1, n = 0; i < chessNo; i++) {
+        if (cycle[(i) % chessNo] <= half)
             n++;
         else n--;
         if (n < 0) {
@@ -105,8 +114,8 @@ bool checkCycle(const array<int, size>& cycle) {
     }
 
     if (!valid_first) {
-        for (int i = 1, n = 0; i < size; i++) {
-            if (cycle[(size - i) % size] <= half)
+        for (int i = 1, n = 0; i < chessNo; i++) {
+            if (cycle[(chessNo - i) % chessNo] <= half)
                 n++;
             else n--;
             if (n < 0) {
@@ -118,15 +127,15 @@ bool checkCycle(const array<int, size>& cycle) {
     return valid_first || valid_second;
 }
 
-void print_nodes(const array<Node, size> nodes) {
+void printNodes(const array<Node, chessNo> nodes) {
     cout << "Cycle: ";
-    const Node* n = &nodes[size - 1];
+    const Node* n = &nodes[chessNo - 1];
     cout << n->val << " ";
     int old1 = n->val;
     n = n->next1;
     cout << n->val << " ";
     int old2 = n->val;
-    for (int i = 0; i < size - 2; i++) {
+    for (int i = 0; i < chessNo - 2; i++) {
         old2 = n->val;
         if (old1 == n->next1->val)
             n = n->next2;
@@ -138,7 +147,7 @@ void print_nodes(const array<Node, size> nodes) {
     cout << endl;
 }
 
-void print_path(const array<int, size> p) {
+void printPath(const array<int, chessNo> p) {
     cout << "Cycle: ";
     for (auto&& e : p) {
         cout << e << " ";
@@ -147,55 +156,59 @@ void print_path(const array<int, size> p) {
 }
 
 int main() {
-
+    std::srand(std::time(nullptr));
     /** Setup OWAL **/
-    for (int i = 1, temp = 0; i <= size; i++) {
-        for (int j = i + 1; j <= size; j++) {
+    for (int i = 1, temp = 0; i <= chessNo; i++) {
+        for (int j = i + 1; j <= chessNo; j++) {
             cout << "(" << i << "," << j << ")";
-            cin >> temp;
+            cin >> temp; 
+            // temp = 1+std::rand()%50;
             owal.push_back({temp, {i, j}});
         }
     }
     sort(owal.begin(), owal.end(), [](Entry& a, Entry& b) {
         return a.weight < b.weight;
     });
-    print_owal(owal);
+    printOwal(owal);
 
     vector<int> com = {};
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < chessNo; i++)
         com.push_back(1);
-    for (int i = 0; i < owal.size() - size; i++)
+    for (int i = 0; i < owal.size() - chessNo; i++)
         com.push_back(0);
 
     next_permutation(com.begin(), com.end());
     vector<int> original_com = com;
 
-    array<int, size> count{};
+    array<int, chessNo> count{};
 
     /** for each k-combination **/
+    int cnt = 0;
     do {
         count.fill(0);
         prev_permutation(com.begin(), com.end());
-//        print_com(com);
+        // printCom(com);
+        if (++cnt % 1000000 == 0) cout << cnt << endl;
         for (int i = 0; i < owal.size(); i++) {
             if (com[i]) {
                 count[owal[i].coords.first - 1]++;
                 count[owal[i].coords.second - 1]++;
             }
         }
-//        print_count(count);
-        for (int i = 0; i < size; i++) {
+//        printCount(count);
+        for (int i = 0; i < chessNo; i++) {
             if (count[i] != 2) break;
-            else if (i == size - 1) {
-                array<Node, size> nodes = genCycle(count, com);
-//                print_com(com);
-//                print_count(count);
-                array<int, size> path = genPath(nodes);
-//                print_path(path);
+            else if (i == chessNo - 1) {
+                array<Node, chessNo> nodes = genCycle(count, com);
+//                printCom(com);
+//                printCount(count);
+                array<int, chessNo> path = genPath(nodes);
+//                printPath(path);
                 if (checkCycle(path)) {
                     cout << "FOUND." << endl;
-                    print_path(path);
+                    printPath(path);
+                    printCom(com);
                     int w = 0;
                     for (int j = 0; j < owal.size(); j++) {
                         if (com[j]) {
@@ -209,4 +222,5 @@ int main() {
             }
         }
     } while (com != original_com);
+
 }
