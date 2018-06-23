35217e8e7b5d7e7539f503b2deb26d6d37617365
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-06-23 17:58:31 +0800

    get rotation

---
+++ openMV/get_rotation.py
@@ -0,0 +1,43 @@
+import sensor, image, time
+from util import dist
+from math import acos
+from pj import *
+from jp import *
+from pm import *
+
+def vote(votes,min_dx, threshold):
+    for v in votes:
+        if(abs(v-min_dx)<threshold):
+            votes[v]+=1
+            return
+    votes[min_dx] = 1
+
+def getHighestVote(votes):
+    max_v_count = 0
+    max_v = 0
+    for v in votes:
+        if votes[v]>max_v_count:
+            max_v_count = votes[v]
+            max_v = v
+    return max_v#acos(max_v/40)
+
+
+def get_rotation(img,rects, threshold):
+    votes = {}
+    for rk, r in enumerate(rects):
+        c = r.corners()
+        min_dx = 10000
+        for i, p1 in enumerate(c):
+            p1 = pm(jp,p1)
+            p2 = pm(jp,c[i-1])
+            # if(abs(dist(p1,p2)-26)>threshold):
+            #     print(p1,p2,dist(p1,p2))
+            #     continue
+            img.draw_circle(p1[0],p1[1],5,color=(255,0,0))
+            if p1[1] > p2[1]:
+                p1, p2 = p2, p1
+            dx = p2[0] - p1[0]
+            if(abs(dx)<abs(min_dx)):
+                min_dx = dx
+        vote(votes,min_dx, threshold)
+    return getHighestVote(votes)
--- openMV/main.py
+++ openMV/main.py
@@ -5,6 +5,7 @@ from fuse_corners import *
 from pj import *
 from jp import *
 from pm import *
+from get_rotation import get_rotation
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565) # grayscale is faster (160x120 max on OpenMV-M7)
@@ -18,27 +19,36 @@ Size = [280,240]
 equations = []
 corners = []
 
+id=0
+
 while(True):
     clock.tick()
     equations = []
     corners=[]
     img = sensor.snapshot()
 
-    for k,r in enumerate(img.find_rects(threshold = 25000)):
+    rects = img.find_rects(threshold = 25000)
+    dx = get_rotation(img,rects,5)
+    print(dx)
+    img.draw_line(80,120,80-dx,80, color=(255,0,0))
+    for k,r in enumerate(rects):
         c = r.corners()
         corners += c
         for i, p in enumerate(c):
             p_ = c[i-1]
-            img.draw_line(p[0],p[1],p_[0],p_[1], color=(255,0,0))
-            #img.draw_circle(p[0], p[1], 5, color = (255, 0, 0))
-    corners = list(map(lambda p:pm(pj,p), corners))
-    print(len(corners),end=" , ")
-    corners = fuse_corners(corners,5)
-    print(len(corners))
-    corners = list(map(lambda p:pm(jp,p), corners))
-    for p in corners:
-        img.draw_circle(p[0], p[1], 5, color = (0, 255, 0))
+            img.draw_line(p[0],p[1],p_[0],p_[1], 5, color=(255,0,0))
+    #         #img.draw_circle(p[0], p[1], 5, color = (255, 0, 0))
+    # corners = list(map(lambda p:pm(pj,p), corners))
+    # print(len(corners),end=" , ")
+    # corners = fuse_corners(corners,5)
+    # print(len(corners))
+    # corners = list(map(lambda p:pm(jp,p), corners))
+    # for p in corners:
+    #     img.draw_circle(p[0], p[1], 5, color = (0, 255, 0))
+
+
     # while(1):
     #     x=0
-
+    # img.save("/img/testing"+str(id)+".bmp")
+    # id+=1
     #printEq(img)
