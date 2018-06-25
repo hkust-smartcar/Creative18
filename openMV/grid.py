4456d38795e021584aecb45322939f19831b1629
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-06-25 21:45:43 +0800

    openMV rotate corners

--- openMV/grid.py
+++ openMV/grid.py
@@ -1,43 +1,49 @@
-import sensor, image, time
+import sensor
+import image
+import time
 from util import dist, mapToImage, mapToWorld, sgn
-from math import acos, pi
+from math import acos, pi, sin, cos
 
-def vote(votes,min_dx, threshold):
+
+def vote(votes, min_dx, threshold):
     for v in votes:
-        if(abs(v-min_dx)<threshold):
-            votes[v]+=1
+        if(abs(v-min_dx) < threshold):
+            votes[v] += 1
             return
     votes[min_dx] = 1
+
 
 def getHighestVote(votes, default):
     max_v_count = 0
     max_v = default
     for v in votes:
-        if votes[v]>max_v_count:
+        if votes[v] > max_v_count:
             max_v_count = votes[v]
             max_v = v
     return max_v
 
-def get_rotation(img,rects, length, threshold):
+
+def get_rotation(img, rects, length, threshold):
     votes = {}
-    d=0
+    d = 0
     for rk, r in enumerate(rects):
         c = r.corners()
         min_dx = 10000
         for i, p1 in enumerate(c):
             p1 = mapToWorld(p1)
             p2 = mapToWorld(c[i-1])
-            img.draw_circle(p1[0],p1[1],5,color=(255,0,0))
+            img.draw_circle(p1[0], p1[1], 5, color=(255, 0, 0))
             if p1[1] > p2[1]:
                 p1, p2 = p2, p1
             dx = p2[0] - p1[0]
-            if(abs(dx)<abs(min_dx)):
+            if(abs(dx) < abs(min_dx)):
                 min_dx = dx
-        vote(votes,min_dx, threshold)
-    d = getHighestVote(votes,d)
+        vote(votes, min_dx, threshold)
+    d = getHighestVote(votes, d)
     print(d)
     print(length)
-    return d,acos(d/length)-3.141592653/2
+    return d, acos(d/length)-3.141592653/2
+
 
 def get_length(img, rects, threshold):
     dist_votes = {}
@@ -47,12 +53,12 @@ def get_length(img, rects, threshold):
         for i, p1 in enumerate(c):
             p1 = mapToWorld(p1)
             p2 = mapToWorld(c[i-1])
-            local_length = dist(p1,p2)
-            if(local_length <20 or local_length >35):
+            local_length = dist(p1, p2)
+            if(local_length < 20 or local_length > 35):
                 break
             vote(dist_votes, local_length, threshold)
     length = getHighestVote(dist_votes, length)
-    print('length,',length)
+    print('length,', length)
     return length
 
 
@@ -61,22 +67,25 @@ def getGoodRects(rects, length, threshold):
     for rect in rects:
         corners = list(map(mapToWorld, rect.corners()))
         reject = False
-        for k,c in enumerate(corners):
-            if(abs(dist(c,corners[k-1]) - length) > threshold):
+        for k, c in enumerate(corners):
+            if(abs(dist(c, corners[k-1]) - length) > threshold):
                 reject = True
                 break
         if not reject:
             goodRects.append(rect)
     return goodRects
 
+
 """
 brief: given previous global and local rotation and current local rotation
 return: current global rotation
 unit: radian
 """
+
+
 def getGlobalRotation(prevg, prevl, currl):
-    currg = prevg- prevl + currl
-    #if (True or abs(prevl) > pi/4 and abs(currl) > pi/4):
+    currg = prevg - prevl + currl
+    # if (True or abs(prevl) > pi/4 and abs(currl) > pi/4):
     if(sgn(prevl) == 1 and sgn(currl) == -1):
         #prev is positive, current is negative
         currg += pi/2
@@ -86,6 +95,36 @@ def getGlobalRotation(prevg, prevl, currl):
     return currg
 
 
+def getGlobalRotationWithDirection(prevg, prevl, currl, clockwise):
+    currg = prevg - prevl + currl
+    if(clockwise):
+        if(currl > prevl):
+            currg -= pi/2
+    else:
+        if(currl < prevl):
+            currg += pi/2
+    return currg
 
 
+def rotateTransform(p, co, si):
+    x = p[0] - 140
+    y = 280 - p[1]
+    x1 = co*x - si*y
+    y1 = si*x + co*y
+    return (int(x1), int(y1))
 
+
+def getRotateCorners(img, fixedCorners, theta):
+    co = cos(theta)
+    si = sin(theta)
+    rotatedCorners = list(map(lambda p: rotateTransform(p, co, si), fixedCorners))
+    return rotatedCorners
+
+
+def getLocalDisplacement(img, rotatedCorners, length):
+    x_votes = {}
+    y_votes = {}
+    for p in rotatedCorners:
+        vote(x_votes, p[0] % length, 2)
+        vote(y_votes, p[1] % length, 2)
+    return [getHighestVote(x_votes, 0), getHighestVote(y_votes, 0)]
--- openMV/main.py
+++ openMV/main.py
@@ -1,25 +1,34 @@
 # main - By: dipsy - Mon Jun 18 2018
 
-import sensor, image, time
+import sensor
+import image
+import time
 from fuse_corners import fuse_corners
 from find_num import find_num
-from grid import get_rotation, get_length, getGoodRects, getGlobalRotation
+from grid import get_rotation,
+get_length,
+getGoodRects,
+getGlobalRotation,
+getGlobalRotationWithDirection,
+getRotateCorners,
+getLocalDisplacement
 from util import mapToWorld, mapToImage, deg
-from math import sin,cos
+from math import sin, cos
 
 sensor.reset()
-sensor.set_pixformat(sensor.RGB565) # grayscale is faster (160x120 max on OpenMV-M7)
+# grayscale is faster (160x120 max on OpenMV-M7)
+sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QQVGA)
-sensor.skip_frames(time = 2000)
+sensor.skip_frames(time=2000)
 clock = time.clock()
 
-size = [160,120]
-Size = [280,240]
+size = [160, 120]
+Size = [280, 240]
 
 equations = []
 corners = []
 
-id=0
+id = 0
 
 gRotation = 0
 lRotation = 0
@@ -27,55 +36,63 @@ lRotation = 0
 while(True):
     clock.tick()
     equations = []
-    corners=[]
+    corners = []
     img = sensor.snapshot()
 
-    rects = img.find_rects(threshold = 25000)
-    #try:
-    length = get_length(img,rects,2)
+    rects = img.find_rects(threshold=25000)
+    # try:
+    length = get_length(img, rects, 2)
     print(length)
     goodRects = getGoodRects(rects, length, 10)
-    dx, theta = get_rotation(img,goodRects,length,2)
-    gRotation = getGlobalRotation(gRotation,lRotation,theta)
+    dx, theta = get_rotation(img, goodRects, length, 2)
+    gRotation = getGlobalRotation(gRotation, lRotation, theta)
+    #gRotation = getGlobalRotationWithDirection(gRotation,lRotation,theta,False)
     lRotation = theta
-    print('deg',deg(gRotation),'theta',theta)
-    #except Exception as e:
-        #print(e)
-        #continue
-    #print(dx,length,theta)
-    [x0,y0] = mapToImage([140,240])
-    [x1,y1] = mapToImage([140-dx,240-length])
-    img.draw_line(x0,y0,x1,y1, color=(255,0,0))
+    print('deg', deg(gRotation), 'theta', theta)
+    # except Exception as e:
+    # print(e)
+    # continue
+    # print(dx,length,theta)
+    [x0, y0] = mapToImage([140, 240])
+    [x1, y1] = mapToImage([140-dx, 240-length])
+    img.draw_line(x0, y0, x1, y1, color=(255, 0, 0))
 
-    [x0,y0] = [70,60]
-    [x1,y1] = [70-int(length*cos(gRotation)),60-int(length*sin(gRotation))]
-    img.draw_line(x0,y0,x1,y1, color=(0,255,0))
+    [x0, y0] = [70, 60]
+    [x1, y1] = [70-int(length*cos(gRotation)), 60-int(length*sin(gRotation))]
+    img.draw_line(x0, y0, x1, y1, color=(0, 255, 0))
 
-    for k,r in enumerate(goodRects):
+    for k, r in enumerate(goodRects):
         c = r.corners()
         corners += c
         for i, p in enumerate(c):
             p_ = c[i-1]
-            img.draw_line(p[0],p[1],p_[0],p_[1], 5, color=(255,0,0))
+            img.draw_line(p[0], p[1], p_[0], p_[1], 5, color=(255, 0, 0))
+
+    fixedCorners = list(map(mapToWorld, corners))
+    fusedCorners = fuse_corners(fixedCorners, 5)
+    rotatedCorners = getRotateCorners(img, fusedCorners, theta)
+    print(rotatedCorners)
+    for p in rotatedCorners:
+        img.draw_circle((p[0]+140)//2, p[1]//2, 5, color=(0, 255, 0))
+    #print(getLocalDisplacement(img,rotatedCorners,))
+    #localDisplacement = getLocalDisplacement(img, rotatedCorners)
     # goodRects = getGoodRects(rects, length, 10)
     # if len(goodRects):
     #     r = goodRects[0]
     #     img.draw_rectangle(r.rect(),color=(0,255,0))
     #     find_num(img,r,theta,length)
 
-
     #         #img.draw_circle(p[0], p[1], 5, color = (255, 0, 0))
-    #corners = list(map(mapToWorld, corners))
+    #
     #print(len(corners),end=" , ")
     #corners = fuse_corners(corners,5)
-    #print(len(corners))
+    # print(len(corners))
     #corners = list(map(mapToImage, corners))
-    #for p in corners:
+    # for p in corners:
         #img.draw_circle(p[0], p[1], 5, color = (0, 255, 0))
-
 
     # while(1):
     #     x=0
     # img.save("/img/testing"+str(id)+".bmp")
     # id+=1
-    #printEq(img)
+    # printEq(img)
